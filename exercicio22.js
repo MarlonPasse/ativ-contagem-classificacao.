ab7a9cd8227f5eb9610c9edb02505105c44c2ee3
Marlon <103782424+MarlonPasse@users.noreply.github.com>
2022-05-15 16:44:21 -0300

    exercicio feito em aula

---
+++ exercicio22.js
@@ -0,0 +1,11 @@
+
+// faça um programa  que imprima  os numeros impares de 0 a 50.
+
+for (let numero = 0; numero <= 50; numero++) {
+
+    if(numero % 2 == 1){ //impar
+
+    console.log(numero);
+    
+}}
+
